034bff7c3a90b57cf8362dcf31828519d0d72add
Michal Majewski <michal_majewski@epam.com>
2023-03-09 19:56:50 +0100

    Prepare a basic REST API using NodeJS and Express

---
+++ node-rest-api/src/people.js
@@ -0,0 +1,15 @@
+export let people = [{
+    name: "John",
+    age: 35,
+    hairColor: "brown",
+},
+{
+    name: "Joe",
+    age: 20,
+    hairColor: "blonde",
+},
+{
+    name: "Jane",
+    age: 40,
+    hairColor: "red",
+}];
