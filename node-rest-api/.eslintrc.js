3a1c5cf0031097d8cc17352b6f55280074d0840e
Michal Majewski <michal_majewski@epam.com>
2023-03-10 10:41:17 +0100

    Add the ESlint

---
+++ node-rest-api/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'immutable',
+  ],
+  'rules': {
+    indent: ["error", 4],
+    'immutable/no-mutation': 'error',
+  },
+};
